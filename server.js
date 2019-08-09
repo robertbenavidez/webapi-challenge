c24e1f381ad7c96e2c4beef801cd3cdcfaa5d9c3
Robert Benavidez <murabitbenavidez@gmail.com>
2019-08-09 14:16:48 -0400

    last commit. did not meet mvp

--- server.js
+++ server.js
@@ -6,14 +6,24 @@ const people = require('./data/people')
 const chores = require('./data/chores')
 
 
+//GET chores by person's ID
+
+server.get('/people/:id/chores', (req, res) =>{
+    res.status(200).json(chores);
+    console.log(req.params.id)
+})
 
 
 
+
+
+// get people
 server.get('/people', (req, res) => {
     res.status(200).json(people);
  });
 
- server.get('/people/chores', (req, res) => {
+ //get chores
+ server.get('/chores', (req, res) => {
     res.status(200).json(chores);
  });
 
